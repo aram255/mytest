881249b70d3964b942b324b09666875a5e130b65
aram255 <36386686+aram255@users.noreply.github.com>
2021-05-26 15:54:16 +0400

    Add files via upload

---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Hello;
+
+
+class HelloWorld
+{
+	public function getSelam()
+	{
+		return "hello world from class";
+	}
+}
---
+++ test/test.php
@@ -0,0 +1,10 @@
+<?php
+
+
+include "../vendor/autoload.php";
+
+use Hello\HelloWorld;
+
+//echo (new Hello)->getSelam();
+
+$a = new HelloWorld;
